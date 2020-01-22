1595d2c49513222ec012caa0b0f10281a2a1da2f
Danny Soto <87dsoto@gmail.com>
2020-01-22 22:08:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ end
 
 def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.detect{|cheese| food.include?(cheese)}
+  food.detect{|food| cheese_types.include?(food)}
 end
